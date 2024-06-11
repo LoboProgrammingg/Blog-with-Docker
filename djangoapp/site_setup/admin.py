a783d897cbb67ad017ddbcd87acc2d350c5cd1ad
lobo <matheusloboo2001@gmail.com>
2024-06-11 14:38:09 -0400

    Adicionando models ao site_setup

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from site_setup.models import MenuLink
+from site_setup.models import MenuLink, SiteSetup
 
 @admin.register(MenuLink)
 class MenuLinkAdmin(admin.ModelAdmin):
     list_display = 'id','text', 'url_or_path',
     list_display_links = 'id', 'text', 'url_or_path',
     search_fields = 'id', 'text', 'url_or_path',
+
+@admin.register(SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display = 'title', 'description'
+    #PARA SABER SE A PESSOA TEM DETERMINADAS PERMISSOES.
+    def has_add_permission(self, request):
+        return not SiteSetup.objects.exists()
