3d35940fc229b77f0f45bbe4d578949ed120c7ee
lobo <matheusloboo2001@gmail.com>
2024-06-11 17:42:34 -0400

    criando novas views

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -5,3 +5,15 @@ def index(request):
         request,
         'blog/pages/index.html',  
     )
+
+def page(request):
+    return render(
+        request,
+        'blog/pages/page.html',  
+    )
+
+def post(request):
+    return render(
+        request,
+        'blog/pages/post.html',  
+    )
