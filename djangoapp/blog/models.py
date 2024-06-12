973257f0639015fd00d4ddf14ab2f20f991582d8
lobo <matheusloboo2001@gmail.com>
2024-06-12 17:18:41 -0400

    Usando pillow para configurar as imagens do post

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from utils.rands import slugify_new
+from utils.images import resize_image
 
 class Tag(models.Model):
     class Meta:
@@ -106,6 +107,13 @@ class Post(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            
             self.slug = slugify_new(self.title, 4)
-        return super().save(*args, **kwargs)
+        current_cover_name = str(self.cover.name)
+        super_save = super().save(*args, **kwargs)
+        cover_changed = False
+
+        if self.cover:
+            cover_changed = current_cover_name != self.cover
+        if cover_changed:
+            resize_image(self.cover, 900, True, 70)
+        return super_save
