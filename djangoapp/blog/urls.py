310d87cb31e8d0c9e2f782724be93f98e1eff93a
lobo <matheusloboo2001@gmail.com>
2024-06-17 17:01:57 -0400

    Configurando o Search

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,4 +1,4 @@
-from blog.views import index, post, page, created_by, category
+from blog.views import index, post, page, created_by, category, tag, search
 from django.urls import path
 
 app_name = 'blog'
@@ -9,4 +9,6 @@ urlpatterns = [
     path('page/<slug:slug>/', page, name='page'),
     path('created_by/<int:author_pk>/', created_by, name='created_by'),
     path('category/<slug:slug>/', category, name='category'),
+    path('tag/<slug:slug>/', tag, name='tag'),
+    path('search/', search, name='search'),
 ] 
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,4 +1,5 @@
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render
 from blog.models import Post
 
@@ -47,6 +48,42 @@ def category(request, slug):
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+        }
+    )
+
+def tag(request, slug):
+    posts = Post.objects.get_published().filter(tags__slug=slug)
+
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def search(request):
+    search_value = request.GET.get('search', '').strip()
+    
+    posts = (
+        Post.objects.get_published()
+        .filter(
+            Q(title__icontains=search_value) |
+            Q(excerpt__icontains=search_value) |
+            Q(content__icontains=search_value)
+        )[:PER_PAGE]
+    )
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': posts,
+            'search_value': search_value,
         }
     )
 
